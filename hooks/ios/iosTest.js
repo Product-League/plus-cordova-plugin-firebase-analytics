13ded6daf645528b0fa1b1802d31ea0b53531796
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2023-09-06 14:57:04 +0100

    Create iosTest.js

---
+++ hooks/ios/iosTest.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var path = require("path");
+
+let xml2js = require('xml2js');
+const fs = require('fs');
+
+var constants = {
+  googleServices: "google-services"
+};
+
+module.exports = function(context) {
+
+  function getAppIdentifier(configPath) {
+    const parseString = xml2js.parseString;
+    const config_xml = fs.readFileSync(configPath).toString();
+    let appId;
+
+    parseString(config_xml, (err, config) => {
+        if (err) return console.error(err);
+
+        console.log("App identifier: " + config['widget']['$'].id);
+        appId = config['widget']['$'].id;
+    })
+    return appId;
+}
+
+  fs.copyFileSync(context.opts.projectRoot + '/platforms/ios/www/' + getAppIdentifier(context.opts.projectRoot + '/platforms/ios/ECOP Mobile/config.xml') + '.firebase' + '/GoogleService-Info.plist' , path.join(context.opts.plugin.dir, 'GoogleService-Info.plist'));
+  console.log("Source path: " + context.opts.projectRoot + '/platforms/ios/www/' + getAppIdentifier(context.opts.projectRoot + '/platforms/ios/ECOP Mobile/config.xml') + '.firebase' + '/GoogleService-Info.plist', "Dest path:" + path.join(context.opts.plugin.dir, 'GoogleService-Info.plist'))
+}
